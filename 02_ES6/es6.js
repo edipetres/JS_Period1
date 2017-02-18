097de17c24a34191eb37cc3952f324b3e4798802
eddie <eddmond@Edmonds-MacBook-Pro.local>
2017-02-18 18:35:25 +0100

    Completing exercises

--- 02_ES6/es6.js
+++ 02_ES6/es6.js
@@ -1,27 +1,87 @@
-/*
-evens = [1,2,3,4,5,6];
+//ES6 Exercises
 
-odds  = evens.map(v => v + 1);
-pairs = evens.map(v => ({even: v, odd: v+1}));
-nums  = evens.map((v,i) => v + i);
+//EX-2 - Using arrow expressions
+evens = [1, 2, 3, 4, 5, 6];
+odds = evens.map(v => v + 1);
+pairs = evens.map(v => ({ even: v, odd: v + 1 }));
+nums = evens.map((v, i) => v + i);
 
-console.log("odds: "+odds)
+console.log("odds: " + odds)
 console.log("Pairs: ")
 pairs.forEach(function (p) { console.log(p) })
-console.log("evens: "+evens)*/
+console.log("evens: " + evens)
 
 
-//EX3 - arrow functions
+//EX-3 A) - arrow functions
+//Losing this when extracting a method
+// -> Since the inner function has a different this from the outside, you need to use a helper variable (that)
 function Numbers(numbs) {
-
   this.nums = numbs;
   this.fives = [];
+  var that = this;
   this.nums.forEach(function (v) {
     if (v % 5 === 0) {
-      this.fives.push(v);   
+      that.fives.push(v);
     }
   });
 }
-var numbers = new Numbers([1,3,5,10,14,20,33,50]);
-console.log(numbers.fives);
+var numbers = new Numbers([1, 3, 5, 10, 14, 20, 33, 50]);
+console.log("\nFives: " + numbers.fives);
+
+//The same using arrow functions:
+function Numbers(numbs) {
+  this.nums = numbs;
+  this.fives = [];
+  var that = this;
+  this.nums.forEach(v => { if (v % 5 === 0) that.fives.push(v) });
+}
+var numbers = new Numbers([1, 3, 5, 10, 14, 20, 33, 50]);
+console.log("Fives with arrow functions: " + numbers.fives);
+
+
+//EX-3 B)
+console.log("\nEX 3. B)")
+
+var counter = {
+  count: 0,
+  inc: function () { this.count++; }
+}
+var func = counter.inc;  //Store "reference" to inc
+func();
+console.log(counter.count);  //Still zero
+counter.inc();
+console.log(counter.count);  //Now one
+
+
+//EX-4 - Template literals
+var customer = { name: "Foo" }
+var card = { amount: 7, product: "Bar", unitprice: 42 }
+var message = `Hello ${customer.name}, want to buy ${card.amount} ${card.product} for a total of ${card.amount * card.unitprice} bucks?`;
+console.log("Message: " + message);
+
+
+//EX-5 A - Rest Parameter and the spread operator
+function myF(x, y, ...rest) {
+  console.log("Sum = " + rest.length)
+  for (i = 0; i < rest.length; i++) {
+    console.log("rest value " + i + " is a " + rest[i].constructor.name);
+  }
+}
+myF(5, 2, true, 2, "hello World", [1, 2, 3], new Date(), {})
+
+//EX-5 B
+var rest = [true, 2, "hello World", [1, 2, 3], new Date(), {}];
+var restParams = [...rest];
+myF(5, 2, ...restParams);
+
+
+//EX-6
+console.log("\nEX-6");
+let fName = "Kurt";
+let lName = "Wonnegut";
+let age = 98
+
+myObj = {fName, lName, age};
+Object.keys(myObj).forEach(k => console.log("Key: "+k + ", value: "+ myObj[k]));
+
 
